3ae28c10d06fc1ad82d06d478d76ae870863e249
Infiloo <67906754+Infiloo@users.noreply.github.com>
2021-01-25 07:31:24 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,36 @@
+input.onButtonPressed(Button.A, function () {
+    bird.change(LedSpriteProperty.Y, -1)
+})
+input.onButtonPressed(Button.B, function () {
+    bird.change(LedSpriteProperty.Y, 1)
+})
+let emptyObstacleY = 0
+let ticks = 0
+let bird: game.LedSprite = null
+let index = 0
+let obstacles: game.LedSprite[] = []
+bird = game.createSprite(0, 2)
+bird.set(LedSpriteProperty.Blink, 300)
+basic.forever(function () {
+    while (obstacles.length > 0 && obstacles[0].get(LedSpriteProperty.X) == 0) {
+        obstacles.removeAt(0).delete()
+    }
+    for (let obstacle2 of obstacles) {
+        obstacle2.change(LedSpriteProperty.X, -1)
+    }
+    if (ticks % 3 == 0) {
+        emptyObstacleY = randint(0, 4)
+        for (let index2 = 0; index2 <= 4; index2++) {
+            if (index2 != emptyObstacleY) {
+                obstacles.push(game.createSprite(4, index2))
+            }
+        }
+    }
+    for (let obstacle3 of obstacles) {
+        if (obstacle3.get(LedSpriteProperty.X) == bird.get(LedSpriteProperty.X) && obstacle3.get(LedSpriteProperty.Y) == bird.get(LedSpriteProperty.Y)) {
+            game.gameOver()
+        }
+    }
+    ticks += 1
+    basic.pause(1000)
+})
